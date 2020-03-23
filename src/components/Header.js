39e5fa36101ca1aca4b16196acfd0f3690abe99c
Miron Banks <madapegarage@gmail.com>
2020-03-23 14:27:48 +0100

    header component

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Logo from '../assets/komplett.png'
+
+const Breadcrumb = () => {
+
+    return (
+        <div className='header'>
+            <div className='header-inner'>
+                <div className='header-content'>
+                    <div className='logo'>
+                        <img src={Logo} alt='Logo' />
+                    </div>
+                    <div className='hamburger-menu'>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Breadcrumb
